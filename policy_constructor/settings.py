74b17808e70388ad11a26c7c32efefc23a1a6e39
Wissstock <omnia.fatum@gmail.com>
2018-12-18 21:40:08 +0200

    requirements.txt update

--- policy_constructor/settings.py
+++ policy_constructor/settings.py
@@ -88,6 +88,13 @@ WSGI_APPLICATION = 'policy_constructor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {                                          #  default DB settings
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
