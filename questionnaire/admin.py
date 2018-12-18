a66afea4fb5eba43361e10f9e0c7b9e9cafff8ec
sashkow <lykhenko.olexandr@gmail.com>
2018-12-18 16:53:02 +0200

    req3

--- questionnaire/admin.py
+++ questionnaire/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Policy
+
+class PolicyAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Policy._meta.fields]
+    exclude = ['ID']
+admin.site.register(Policy, PolicyAdmin)
+
 
 # Register your models here.
--- questionnaire/models.py
+++ questionnaire/models.py
@@ -4,11 +4,19 @@ from django.db import models
 # form entry to policies
 #
 
-# class Policy(models.Model):
-#     pass
+class Policy(models.Model):
+    form_slug = models.SlugField(default="golosuvalka")
+    category = models.CharField(max_length=200, null=True, blank=True)
+    subcategory = models.CharField(max_length=200, null=True, blank=True)
+    question = models.CharField(max_length=1000)
+    answer = models.CharField(max_length=1000)
+    policy = models.TextField()
+    party = models.CharField(max_length=200, null=True, blank=True)
+
+
 
     # form slug
-    # field slug
+    # question
     # answer
     # policy
 # Create your models here.
