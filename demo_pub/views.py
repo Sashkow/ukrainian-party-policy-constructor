13531dbed3002faaf156c81199e1a804f7d392b6
Wissstock <omnia.fatum@gmail.com>
2018-12-19 23:00:05 +0200

    Questionnaire mod

--- demo_pub/views.py
+++ demo_pub/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def demo_form(request):
+    return render(request, 'demo_pub/demo_form.html', {})
--- questionnaire/admin.py
+++ questionnaire/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import demo_model
+
+admin.site.register(demo_model)
+
 # Register your models here.
